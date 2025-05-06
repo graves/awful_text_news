ad6c36e20ac3b294cc5a2895daa9ce2f52057126
Thomas Gentry <thomas@awfulsec.com>
2025-05-06 03:06:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,233 @@
+# Awful Text News
+
+```sh
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢸⣿⣿⣶⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⣶⣶⠿⣿⡇⠀⠀
+⠀⠀⢸⣿⣈⣉⠙⠛⠻⠿⣿⣶⣤⡀⠀⠀⢀⣤⣶⠿⠛⠋⠉⠀⠀⠀⢸⡇⠀⠀
+⠀⠀⢸⣿⠛⠻⣿⢷⣶⣦⣤⣈⡉⣿⡇⢸⡟⠉⠀⠀⠀⠀⠀⠀⠀⢀⣼⡇⠀⠀
+⠀⠀⢸⣿⠿⠶⣿⣤⣴⣿⣏⣉⣙⣿⡇⢸⡇⠀⠀⣀⣀⣤⣴⣶⠿⠿⣿⡇⠀⠀
+⠀⠀⢸⣷⣶⣤⣤⣤⣄⣈⣉⠙⠛⣿⡇⢸⣷⠾⠟⠛⢉⣿⣧⣤⣴⣶⣿⡇⠀⠀
+⠀⠀⢸⣯⣤⣄⣸⣿⣏⠙⠛⠛⠛⣿⡇⢸⣿⣴⣶⡿⠿⠛⠛⣿⣇⣤⣽⡇⠀⠀
+⠀⠀⢸⡏⠉⠛⠛⠛⢿⡿⠿⢿⣶⣿⡇⢸⣿⣉⣤⣤⡶⠾⠛⠛⢉⣉⣽⡇⠀⠀
+⠀⠀⢸⡇⠀⠀⠀⠀⢸⡷⠶⢤⣤⣿⡇⢸⣿⣉⣥⣿⣶⣶⠞⠛⠋⠉⢹⡇⠀⠀
+⠀⠀⢸⡇⠀⠀⠀⠀⢸⣷⣦⣤⣤⣿⡇⢸⣿⠋⣉⣉⣨⣿⠀⣿⣿⡇⢸⡇⠀⠀
+⠀⠀⢸⣇⣀⣀⣀⡀⢸⣧⣄⣉⣉⣿⡇⢸⣿⠛⠋⣉⣹⣿⣀⣉⣉⣠⣼⡇⠀⠀
+⠀⠀⠈⠉⠉⠉⠉⠛⠛⠛⠿⠿⢿⣿⡇⢸⣿⡿⠿⠿⠛⠛⠛⠉⠉⠉⠉⠁⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+```
+
+Awful Text News uses the [awful_aj](https://github.com/graves/awful_aj) OpenAI-compatible API client library to summarize newspaper articles from sources that publish text-only or _lite_ versions of their stories.
+
+**awful_aj** supports _"Tool use"_ which allows us to specify a JSON Schema that the LLM will conform to. This is specified in a template file. An example file is available [here](./news_parser.yaml). You can specify the conversation's options in a configuration file. It must be `yaml` and an example is available [here](./config.yaml).
+
+## Installation
+
+[Install rust.](https://www.rust-lang.org/tools/install)
+
+```sh
+cargo install awful_text_news
+```
+
+## Configuration
+
+The important configuration options in `config.yaml` to adjust are:
+- `api_key`: Your OpenAI-compatible API's key if it is protected by one.
+- `api_base`: The URL where your OpenAI-compatible API is located. _Default setups require the `/v1` at the end.
+- `model`: This specifies the LLM to use. This project was built using [Qwen_Qwen2.5-3B-Instruct-GGUF](https://huggingface.co/Qwen/Qwen2.5-3B-Instruct). The JSON Schema was adjusted until the model was able to output a mostly consistent representation of the data I was looking for. Experimentation with other models is highly recommended. Please let me know your experience if you do.
+
+The rest of the configuration options can be safely ignored as they are specific to `awful_aj`'s vector store support that allows details of the conversation to be recalled once they are no longer in the context window. This project relies on one-shot instructions.
+
+The `news_parser.yaml` is a template file that specifies the system prompt, along with counterfeit user and assistant messages to guide the LLM into a style of correspondence or restrict output to a format. I found including at least one example of what I actually expect the output to be, greatly improves the results.
+
+The application expects `config.yaml` to be in `com.awful-sec.aj` in your platform's system configuration directory, or `XDG_DIR` in linux, and `news_parser.yaml` to be in a subdirectory named `templates`.
+
+```sh
+λ tree `/Users/tg/Library/Application Support/com.awful-sec.aj`
+/Users/tg/Library/Application Support/com.awful-sec.aj
+├── config.yaml
+└── templates
+    ├── news_parser.yaml
+```
+
+## Use
+
+### Run
+
+```sh
+awful_text_news  --json-output-dir . --markdown-output-dir /Users/tg/Projects/awful_security_news/src
+```
+
+### Expected output
+
+```sh
+Indexed 100 article urls from https://lite.cnn.com
+Indexed 20 article urls from https://text.npr.org
+Fetched 100 article contents from CNN
+Fetched 20 article contents from NPR
+Wrote JSON API file to ./2025-05-06/evening.json
+Processed 1/120 articles
+Processed 2/120 articles
+Processed 3/120 articles
+Processed 4/120 articles
+Processed 5/120 articles
+Processed 6/120 articles
+Processed 7/120 articles
+Processed 8/120 articles
+Processed 9/120 articles
+Processed 10/120 articles
+Processed 11/120 articles
+Processed 12/120 articles
+Processed 13/120 articles
+Processed 14/120 articles
+Processed 15/120 articles
+Processed 16/120 articles
+Processed 17/120 articles
+Processed 18/120 articles
+Processed 19/120 articles
+Processed 20/120 articles
+Processed 21/120 articles
+Processed 22/120 articles
+Processed 23/120 articles
+Processed 24/120 articles
+Processed 25/120 articles
+Processed 26/120 articles
+Processed 27/120 articles
+Processed 28/120 articles
+Processed 29/120 articles
+Processed 30/120 articles
+Processed 31/120 articles
+Processed 32/120 articles
+Processed 33/120 articles
+Processed 34/120 articles
+Processed 35/120 articles
+Processed 36/120 articles
+Processed 37/120 articles
+Processed 38/120 articles
+Processed 39/120 articles
+Processed 40/120 articles
+Processed 41/120 articles
+Processed 42/120 articles
+Processed 43/120 articles
+Processed 44/120 articles
+Processed 45/120 articles
+Processed 46/120 articles
+Processed 47/120 articles
+Processed 48/120 articles
+Processed 49/120 articles
+Processed 50/120 articles
+Processed 51/120 articles
+Processed 52/120 articles
+Processed 53/120 articles
+Processed 54/120 articles
+Processed 55/120 articles
+Processed 56/120 articles
+Processed 57/120 articles
+Processed 58/120 articles
+Processed 59/120 articles
+Processed 60/120 articles
+Processed 61/120 articles
+Processed 62/120 articles
+Processed 63/120 articles
+Processed 64/120 articles
+Processed 65/120 articles
+Processed 66/120 articles
+Processed 67/120 articles
+Processed 68/120 articles
+Processed 69/120 articles
+Processed 70/120 articles
+Processed 71/120 articles
+Processed 72/120 articles
+Processed 73/120 articles
+Processed 74/120 articles
+Processed 75/120 articles
+Processed 76/120 articles
+Processed 77/120 articles
+Processed 78/120 articles
+Processed 79/120 articles
+Processed 80/120 articles
+Processed 81/120 articles
+Processed 82/120 articles
+Processed 83/120 articles
+Processed 84/120 articles
+Processed 85/120 articles
+Processed 86/120 articles
+Processed 87/120 articles
+Processed 88/120 articles
+Processed 89/120 articles
+Processed 90/120 articles
+Processed 91/120 articles
+Processed 92/120 articles
+Processed 93/120 articles
+Processed 94/120 articles
+Processed 95/120 articles
+Processed 96/120 articles
+Processed 97/120 articles
+Processed 98/120 articles
+Processed 99/120 articles
+Processed 100/120 articles
+Processed 101/120 articles
+Processed 102/120 articles
+Processed 103/120 articles
+Processed 104/120 articles
+Processed 105/120 articles
+Processed 106/120 articles
+Processed 107/120 articles
+Processed 108/120 articles
+Processed 109/120 articles
+Processed 110/120 articles
+Processed 111/120 articles
+Processed 112/120 articles
+Processed 113/120 articles
+Processed 114/120 articles
+Processed 115/120 articles
+Processed 116/120 articles
+Processed 117/120 articles
+Processed 118/120 articles
+Processed 119/120 articles
+Processed 120/120 articles
+Wrote FrontPage to /Users/tg/Projects/awful_security_news/src/2025-05-06_evening.md
+Execution time: 1372.27s (1372.269 seconds)
+```
+
+This will fetch the news articles from [https://lite.cnn.com](https://lite.cnn.com) and [https://text.npr.org](https://text.npr.org), then send their contents to be summarized. The model will also extract **named entities**, **key takeaways**, **important dates**, and **important timeframes**.
+
+`awful_text_news` will write a `json` file to the location specified by the `--json-output-dir` flag. This file will be written to a subdirectory named after the date of execution and the `json` file itself will be named after the time of day.
+
+```sh
+λ cat ./2025-05-06/evening.json | jq | head -n 10
+{
+  "local_date": "2025-05-06",
+  "time_of_day": "evening",
+  "local_time": "02:08:07.627114",
+  "articles": [
+    {
+      "source": "https://lite.cnn.com/2025/05/06/asia/us-philippines-exercise-target-ship-sinks-intl-hnk-ml",
+      "dateOfPublication": "2025-05-06",
+      "timeOfPublication": "02:03:00.000Z",
+      "title": "A former US World War II-era warship sank before US and Philippine forces could use in drills",
+```
+
+This file is overwritten with an additional article every time one is processed. This allows us to use the file as a real-time API.
+
+`awful_text_news` will also write a `markdown` representation to the location specified by the `--markdown-output-dir` flag. This file will be named `todays-date_time_of_day.md`. It allows the content to be read without distraction.
+
+
+```sh
+λ mdcat /Users/tg/Projects/awful_security_news/src/2025-05-06_evening.md | head -n 20
+┄Awful Times
+
+┄┄┄┄Edition published at 02:08:07.627114
+
+┄┄A former US World War II-era warship sank before US and Philippine forces could use in drills
+
+• source
+• Published: 2025-05-06 02:03:00.000Z
+
+┄┄┄Summary
+
+A 1944 World War II-era US ship, the ex-USS Brattleboro, was scheduled to be used as the main target in the US-Philippine joint military drills, but it unexpectedly sank before the exercise could take place. This incident occurred in an area facing the disputed Scarborough Shoal, which has been the site of increasing tensions between China and the Philippines. The USS Brattleboro, which participated in crucial battles during World War II, sank at 7:20 AM local time on Monday, April 28, 2025. The ship, designated as a submarine chaser, served in important roles in the Battle of Leyte and the invasion of Okinawa. Despite the ship's age and unsuitability for normal operations, it was selected as the target for the MARSTRIKE exercise. The Philippine and US joint task forces will still achieve their training objectives, as other elements of the exercise were still scheduled to occur. The Philippine military stated that there was no environmental danger from the sinking, as the vessel had been cleaned before being towed out for the exercise. This event highlights the importance of maintaining and preserving historical military assets while also addressing the challenges posed by the potential risks involved in using such assets for military exercises.
+```
+
+The entire process takes about `1372.269 seconds` to run when the `Qwen2.5-3B-Instruct` model is ran using [llamacpp](https://github.com/ggml-org/llama.cpp) on Google Collab's A100 GPU. There is an `ipynb` [here](./Awful\ News\ Llama\ A100.ipynb) for automatically deploying `llama.cpp` using the `Qwen2.5-3B-Instruct` model and `cloudflared` to provide the public URL for your `api_base` configuration.
---
+++ src/main.rs
@@ -0,0 +1,351 @@
+use awful_aj::api::ask;
+use awful_aj::config;
+use awful_aj::config_dir;
+use awful_aj::template;
+use chrono::Local;
+use chrono::NaiveTime;
+use futures::stream::{self, StreamExt};
+use reqwest::get;
+use scraper::{Html, Selector};
+use serde::Deserialize;
+use serde::Serialize;
+use std::error::Error;
+use std::fmt::Write;
+use tokio::fs;
+use url::Url;
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Cli {
+    /// Output directory for the JSON API file
+    #[arg(short, long)]
+    json_output_dir: String,
+
+    /// Output directory for the Markdown file
+    #[arg(short, long)]
+    markdown_output_dir: String,
+}
+
+#[derive(Debug)]
+struct NewsArticle {
+    source: String,
+    content: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct FrontPage {
+    local_date: String,
+    time_of_day: String,
+    local_time: String,
+    articles: Vec<AwfulNewsArticle>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct AwfulNewsArticle {
+    pub source: Option<String>,
+    pub dateOfPublication: String,
+    pub timeOfPublication: String,
+    pub title: String,
+    pub summaryOfNewsArticle: String,
+    pub keyTakeAways: Vec<String>,
+    pub namedEntities: Vec<NamedEntity>,
+    pub importantDates: Vec<ImportantDate>,
+    pub importantTimeframes: Vec<ImportantTimeframe>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct NamedEntity {
+    pub name: String,
+    pub whatIsThisEntity: String,
+    pub whyIsThisEntityRelevantToTheArticle: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ImportantDate {
+    pub dateMentionedInArticle: String,
+    pub descriptionOfWhyDateIsRelevant: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ImportantTimeframe {
+    pub approximateTimeFrameStart: String,
+    pub approximateTimeFrameEnd: String,
+    pub descriptionOfWhyTimeFrameIsRelevant: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let start_time = std::time::Instant::now();
+
+    let args = Cli::parse();
+
+    let cnn_page_url = "https://lite.cnn.com";
+    let cnn_base_url = Url::parse(cnn_page_url).expect("Invalid base URL");
+
+    let npr_page_url = "https://text.npr.org";
+    let npr_base_url = Url::parse(npr_page_url).expect("Invalid base URL");
+
+    // Fetch CNN Lite front page
+    let cnn_html = get(cnn_page_url).await?.text().await?;
+    let cnn_document = Html::parse_document(&cnn_html);
+    let cnn_story_selector = Selector::parse(".card--lite a[href]").unwrap();
+    let mut cnn_article_urls = Vec::new();
+    for element in cnn_document.select(&cnn_story_selector) {
+        if let Some(href) = element.value().attr("href") {
+            if let Ok(resolved) = cnn_base_url.join(href) {
+                cnn_article_urls.push(resolved.to_string());
+            }
+        }
+    }
+
+    println!(
+        "Indexed {} article urls from {}",
+        cnn_article_urls.len(),
+        cnn_page_url
+    );
+
+    // Fetch NPR Lite front page
+    let npr_html = get(npr_page_url).await?.text().await?;
+    let npr_document = Html::parse_document(&npr_html);
+    let npr_story_selector = Selector::parse(".topic-title").unwrap();
+    let mut npr_article_urls = Vec::new();
+    for element in npr_document.select(&npr_story_selector) {
+        if let Some(href) = element.value().attr("href") {
+            if let Ok(resolved) = npr_base_url.join(href) {
+                npr_article_urls.push(resolved.to_string());
+            }
+        }
+    }
+
+    println!(
+        "Indexed {} article urls from {}",
+        npr_article_urls.len(),
+        npr_page_url
+    );
+
+    // Independently fetch each article from CNN
+    let cnn_articles: Vec<NewsArticle> = stream::iter(cnn_article_urls)
+        .filter_map(|url: String| async move { fetch_cnn_article(&url).await.unwrap() })
+        .collect()
+        .await;
+
+    println!("Fetched {} article contents from CNN", cnn_articles.len());
+
+    // Independently fetch each article from NPR
+    let npr_articles: Vec<NewsArticle> = stream::iter(npr_article_urls)
+        .filter_map(|url: String| async move { fetch_npr_article(&url).await.unwrap() })
+        .collect()
+        .await;
+
+    println!("Fetched {} article contents from NPR", npr_articles.len());
+
+    let articles = vec![cnn_articles, npr_articles]
+        .into_iter()
+        .flatten()
+        .collect::<Vec<NewsArticle>>();
+
+    let template = template::load_template("news_parser").await?;
+    let conf_file = config_dir()?.join("config.yaml");
+    let config =
+        config::load_config(conf_file.to_str().expect("Not a valid config filename")).unwrap();
+
+    let local_date = Local::now().date_naive().to_string();
+    let local_time = Local::now().time().to_string();
+    let mut front_page = FrontPage {
+        time_of_day: time_of_day(),
+        local_time: local_time,
+        local_date: local_date,
+        articles: Vec::new(),
+    };
+
+    // Output
+    let mut processed_count = 0;
+    for article in &articles {
+        let response_json = ask(&config, article.content.clone(), &template, None, None).await?;
+        let awful_news_article: Result<AwfulNewsArticle, serde_json::Error> =
+            serde_json::from_str(&response_json);
+
+        if awful_news_article.is_ok() {
+            let mut awful_news_article = awful_news_article.unwrap();
+            awful_news_article.source = Some(article.source.clone());
+            front_page.articles.push(awful_news_article);
+
+            let json = serde_json::to_string(&front_page).unwrap();
+
+            let api_file_dir = format!("{}", front_page.local_date);
+            fs::create_dir_all(&api_file_dir).await?;
+
+            let full_json_dir = format!("{}/{}", args.json_output_dir, front_page.local_date);
+            fs::create_dir_all(&full_json_dir).await?;
+
+            let output_json_filename = format!("{}/{}.json", full_json_dir, front_page.time_of_day);
+            fs::write(&output_json_filename, json).await?;
+            if processed_count == 0 {
+                println!("Wrote JSON API file to {}", output_json_filename);
+            }
+        }
+
+        processed_count += 1;
+        println!("Processed {}/{} articles", processed_count, articles.len());
+    }
+
+    let markdown = front_page_to_markdown(&front_page);
+    let output_markdown_filename = format!("{}/{}_{}.md", args.markdown_output_dir, front_page.local_date, front_page.time_of_day);
+    fs::write(&output_markdown_filename, markdown).await?;
+
+    println!("Wrote FrontPage to {}", output_markdown_filename);
+
+    let elapsed = start_time.elapsed();
+    println!(
+        "Execution time: {:.2?} ({}.{:03} seconds)",
+        elapsed,
+        elapsed.as_secs(),
+        elapsed.subsec_millis()
+    );
+
+    Ok(())
+}
+
+pub fn time_of_day() -> String {
+    let morning_low = NaiveTime::from_hms_opt(4, 00, 0).unwrap();
+    let morning_high = NaiveTime::from_hms_opt(12, 00, 0).unwrap();
+
+    let afternoon_low = NaiveTime::from_hms_opt(12, 00, 0).unwrap();
+    let afternoon_high = NaiveTime::from_hms_opt(8, 00, 0).unwrap();
+
+    let _evening_low = NaiveTime::from_hms_opt(8, 00, 0).unwrap();
+    let _evening_high = NaiveTime::from_hms_opt(4, 00, 0).unwrap();
+
+    let time_of_day = Local::now().time();
+
+    if (time_of_day >= morning_low) && (time_of_day < morning_high) {
+        "morning".to_string()
+    } else if (time_of_day >= afternoon_low) && (time_of_day < afternoon_high) {
+        "afternoon".to_string()
+    } else {
+        "evening".to_string()
+    }
+}
+
+async fn fetch_cnn_article(url: &str) -> Result<Option<NewsArticle>, Box<dyn Error>> {
+    // Attempt to fetch and parse the page
+    let body = get(url).await?.text().await?;
+
+    let document = Html::parse_document(&body);
+    let mut content = String::new();
+    let headline_selector = Selector::parse(".headline--lite")?;
+    let article_selector = Selector::parse(".article--lite")?;
+
+    for element in document
+        .select(&headline_selector)
+        .chain(document.select(&article_selector))
+    {
+        let text = element.text().collect::<Vec<_>>().join(" ");
+        content.push_str(&text);
+        content.push_str("\n");
+    }
+
+    Ok(Some(NewsArticle {
+        source: url.to_string(),
+        content,
+    }))
+}
+
+async fn fetch_npr_article(url: &str) -> Result<Option<NewsArticle>, Box<dyn Error>> {
+    // Attempt to fetch and parse the page
+    let body = get(url).await?.text().await?;
+
+    let document = Html::parse_document(&body);
+    let mut content = String::new();
+    let headline_selector = Selector::parse(".story-head")?;
+    let article_selector = Selector::parse(".paragraphs-container")?;
+
+    for element in document
+        .select(&headline_selector)
+        .chain(document.select(&article_selector))
+    {
+        let text = element.text().collect::<Vec<_>>().join(" ");
+        content.push_str(&text);
+        content.push_str("\n");
+    }
+
+    Ok(Some(NewsArticle {
+        source: url.to_string(),
+        content,
+    }))
+}
+
+pub fn front_page_to_markdown(front_page: &FrontPage) -> String {
+    let mut md = String::new();
+    writeln!(md, "# Awful Times\n").unwrap();
+    writeln!(md, "#### Edition published at {}\n", front_page.local_time).unwrap();
+
+    for article in &front_page.articles {
+        writeln!(md, "## {}\n", article.title).unwrap();
+
+        if let Some(source) = &article.source {
+            writeln!(md, "- [source]({})", source).unwrap();
+        }
+        writeln!(
+            md,
+            "- _Published: {} {}_\n",
+            article.dateOfPublication, article.timeOfPublication
+        )
+        .unwrap();
+
+        writeln!(md, "### Summary\n").unwrap();
+        writeln!(md, "{}\n", article.summaryOfNewsArticle.trim()).unwrap();
+
+        if !article.keyTakeAways.is_empty() {
+            writeln!(md, "### Key Takeaways").unwrap();
+            for takeaway in &article.keyTakeAways {
+                writeln!(md, "  - {}", takeaway).unwrap();
+            }
+            writeln!(md).unwrap();
+        }
+
+        if !article.namedEntities.is_empty() {
+            writeln!(md, "### Named Entities").unwrap();
+            for entity in &article.namedEntities {
+                writeln!(md, "- **{}**", entity.name).unwrap();
+                writeln!(md, "    - {}", entity.whatIsThisEntity).unwrap();
+                writeln!(md, "    - {}", entity.whyIsThisEntityRelevantToTheArticle).unwrap();
+            }
+            writeln!(md).unwrap();
+        }
+
+        if !article.importantDates.is_empty() {
+            writeln!(md, "### Important Dates").unwrap();
+            for date in &article.importantDates {
+                writeln!(md, "  - **{}**", date.dateMentionedInArticle).unwrap();
+                writeln!(md, "    - {}", date.descriptionOfWhyDateIsRelevant).unwrap();
+            }
+            writeln!(md).unwrap();
+        }
+
+        if !article.importantTimeframes.is_empty() {
+            writeln!(md, "### Important Timeframes").unwrap();
+            for timeframe in &article.importantTimeframes {
+                writeln!(
+                    md,
+                    "  - **From _{}_ to _{}_**",
+                    timeframe.approximateTimeFrameStart, timeframe.approximateTimeFrameEnd
+                )
+                .unwrap();
+                writeln!(
+                    md,
+                    "    - {}",
+                    timeframe.descriptionOfWhyTimeFrameIsRelevant
+                )
+                .unwrap();
+            }
+            writeln!(md).unwrap();
+        }
+
+        writeln!(md, "---\n").unwrap();
+    }
+
+    md
+}
